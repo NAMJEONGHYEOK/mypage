fe3ce56f97ddd45f9fd24da549bd68a3ac2a7213
NAM JEONG HYEOK <namjh654@naver.com>
2022-04-23 19:47:56 +0900

    first setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,13 @@ const Home: NextPage = () => {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.tsx</code>
+        </p>        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.tsx</code>
+        </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
