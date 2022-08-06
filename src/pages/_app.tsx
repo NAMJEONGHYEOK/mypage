949273044f36057cdd82786489e0eaaf762073c6
NAM JEONG HYEOK <namjh654@naver.com>
2022-08-06 17:59:10 +0900

    next-auth+로그인 예제

---
+++ src/pages/_app.tsx
@@ -0,0 +1,36 @@
+import '../../styles/globals.css'
+import type {AppProps} from 'next/app'
+import Head from 'next/head';
+import {SessionProvider} from "next-auth/react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../../src/theme';
+import createEmotionCache from '../../src/createEmotionCache';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <SessionProvider session={pageProps.session} refetchInterval={5 * 60} refetchOnWindowFocus={true}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </SessionProvider>
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
